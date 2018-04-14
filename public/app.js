4d40052fbf0e49a727ac984b5fa81a34910a3873
DeepCFoxXx <learncode23@gmail.com>
2018-04-14 15:00:28 +0100

    add quote added

--- public/app.js
+++ public/app.js
@@ -3,6 +3,14 @@ var app = function() {
   addQuote("Darth Sidious", "Do it");
 };
 
+var addQuote = function(author, text) {
+  var quoteArticle = createQuoteArticle();
+  var blockquote = createBlockQuote(text);
+  var cite = createCite(author);
+  var quotes = document.querySelector('#quotes');
+  appendElements(quotes, quoteArticle, blockquote, cite);
+};
+
 var createQuoteArticle = function() {
   var quoteArticle = document.createElement('article');
   quoteArticle.classList.add('quote');
